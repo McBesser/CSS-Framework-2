dd97300daacb78a8e6e028be50b42b74bd397dcc
McBesser <53328146+McBesser@users.noreply.github.com>
2024-02-25 11:16:42 +0100

    Update CSSF.js

--- dist/CSSF.js
+++ dist/CSSF.js
@@ -20,6 +20,7 @@ class CSSF {
             'minmax': 'minmax(§0, §1)',
             'repeat': 'repeat(§0, §1)',
             'repeat-minmax': 'repeat(§0, minmax(§1, §2))',
+            'overlay': 'pos_fixed--d_none--w100p--h100p--t0--l0--r0--b0--tpl-rgba_bg_§0_§1_§2_§3c--z_2--cur_pointer',
          },
          settings.templates || {}
       );
@@ -85,8 +86,8 @@ class CSSF {
             'clamp-font-size': 'tpl-clamp-size-standard_font-size_var-cfs-font-size_var-cfs-width',
             'btn': 'px15--py10--cursor_pointer--br3_solid_var-btn-br-color',
             'visually-hidden': 'pos_absolute--h1px--w1px--of_hidden--tpl-rect_clip_1px_1px_1px_1px--ws_nowrap',
-            'focusable': 'target-pseudo-class-focus--pos_static--h_auto--w_auto--of_visible--clip_auto',
-            'overlay': 'pos_fixed--d_none--w100p--h100p--t0--l0--r0--b0--tpl-rgba_bg_0_0_0_50c--z_2--cur_pointer'
+            'focusable': 'target-pseudo-class-focus',
+            'overlay': 'tpl-overlay_0_0_0_50'
          },
          settings.alias || {}
       );
